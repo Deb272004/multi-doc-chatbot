27469a3fcd462d3ce23e66cd7988d7d05f109d55
DEBLINA GUPTA <deblina.gupta2004@gmail.com>
2025-12-25 20:59:08 +0530

    exception module added

--- multi-doc-chat/logger/custom_logger.py
+++ multi-doc-chat/logger/custom_logger.py
@@ -5,48 +5,35 @@ import structlog
 
 
 class CustomLogger:
-    _configured = False  
+    def __init__(self, log_dir="logs", level=logging.INFO):
+        os.makedirs(log_dir, exist_ok=True)
 
-    def __init__(self, log_dir="logs"):
-        self.logs_dir = os.path.join(os.getcwd(), log_dir)
-        os.makedirs(self.logs_dir, exist_ok=True)
-
-        log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-        self.log_file_path = os.path.join(self.logs_dir, log_file)
-
-        
-        if not CustomLogger._configured:
-            self._configure()
-            CustomLogger._configured = True
-
-    def _configure(self):
-        file_handler = logging.FileHandler(self.log_file_path)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(logging.Formatter("%(message)s"))
-
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(logging.Formatter("%(message)s"))
-
-        root_logger = logging.getLogger()
-        root_logger.setLevel(logging.INFO)
+        log_file = os.path.join(
+            log_dir,
+            f"{datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+        )
 
         
-        if not root_logger.handlers:
-            root_logger.addHandler(console_handler)
-            root_logger.addHandler(file_handler)
+        logging.basicConfig(
+            level=level,
+            format="%(message)s",
+            handlers=[
+                logging.FileHandler(log_file),
+                logging.StreamHandler(),
+            ],
+        )
 
+        
         structlog.configure(
             processors=[
                 structlog.processors.TimeStamper(fmt="iso", utc=True),
                 structlog.processors.add_log_level,
-                structlog.processors.EventRenamer(to="event"),
+                structlog.processors.EventRenamer("event"),
                 structlog.processors.JSONRenderer(),
             ],
             logger_factory=structlog.stdlib.LoggerFactory(),
             cache_logger_on_first_use=True,
         )
 
-    def get_logger(self, name=__file__):
-        logger_name = os.path.basename(name)
-        return structlog.get_logger(logger_name)
+    def get_logger(self, name: str):
+        return structlog.get_logger(name)
